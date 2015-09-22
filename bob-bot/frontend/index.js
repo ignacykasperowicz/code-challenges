5770ee82983e724eb9def60d9875c3ed4dfcf365
Ignacy Kasperowicz <ignacy.kasperowicz@gmail.com>
2015-09-22 10:28:09 +0200

    Human friendly and randomized usernames

--- bob-bot/frontend/index.js
+++ bob-bot/frontend/index.js
@@ -1,27 +1,39 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var _ = require('lodash');
 var users = [];
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+var userName = function() {
+  return 'user' + Math.floor(Math.random()*9000);
+};
+
 io.on('connection', function(socket){
-  users.push(socket.id);
+  users.push({name: userName(), socket: socket.id});
 
   io.emit('connected',
     { users: users, user: socket.id }
   );
 
   socket.on('disconnect', function() {
-    users.splice(users.indexOf(socket.id), 1);
+    _.remove(users, {
+      socket: socket.id
+    });
+
     io.emit('disconnected',
       { users: users }
     );
   });
 
   socket.on('chat message', function(data){
+    var user = _.find(users, function(user) {
+      return user.socket == data.socket;
+    });
+    data['name'] = user.name;
     io.emit('chat message', data);
   });
 });
