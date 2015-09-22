921d9e2fba4ffb5b7c76de48808bc3eb121dcd7f
Ignacy Kasperowicz <ignacy.kasperowicz@gmail.com>
2015-09-22 09:54:10 +0200

    Add Bootstrap, Lodash; Add users list

--- bob-bot/frontend/index.js
+++ bob-bot/frontend/index.js
@@ -1,14 +1,29 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var users = [];
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
+  users.push(socket.id);
+
+  io.emit('connected',
+    { users: users }
+  );
+
+  socket.on('disconnect', function() {
+    users.splice(users.indexOf(socket.id), 1);
+    io.emit('disconnected',
+      { users: users }
+    );
+  });
+
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+    console.log(users);
   });
 });
 
